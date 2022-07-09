37fd5f69934f974df37c9f157c049dc63501ccee
zapan1015 <107781007+zapan1015@users.noreply.github.com>
2022-07-09 12:40:33 +0900

    Update install.sh

--- Docker/install.sh
+++ Docker/install.sh
@@ -5,4 +5,16 @@ apt-get upgrade -y
 apt-get install -y bzip2 gcc git htop screen vim wget
 apt-get clean
 
+# Miniconda 설치 파일
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh
+# Miniconda 설치
+bash Miniconda.sh -b
+rm -f Miniconda.sh
+
+cat <<EOF >> ~/.profile 
+
+if [ -d /root/miniconda3/bin ]; then
+ PATH="/root/miniconda3/bin:$PATH"
+fi
+EOF
+	
