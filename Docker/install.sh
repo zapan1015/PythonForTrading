dd0467f3ec83e75d9564684557ff21884f4f0dc4
zapan1015 <107781007+zapan1015@users.noreply.github.com>
2022-07-09 12:26:50 +0900

    Update install.sh

--- Docker/install.sh
+++ Docker/install.sh
@@ -4,3 +4,5 @@ apt-get update
 apt-get upgrade -y
 apt-get install -y bzip2 gcc git htop screen vim wget
 apt-get clean
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O Miniconda.sh
